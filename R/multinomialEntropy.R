e04efc08264419deb4e5ea7d8e05c0574ee5e986
CodesByChris <czingg@ethz.ch>
2018-12-14 15:38:44 +0100

    Improved entropy computation accuracy.

--- R/multinomialEntropy.R
+++ R/multinomialEntropy.R
@@ -1,7 +1,17 @@
 logfactorial <- Vectorize(function(n){
-  if(n<25) return(log(factorial(n)))
+  if(n<170) return(log(factorial(n)))
   return(1/2*log(2*pi) + (n+1/2)*log(n) - n)
 })
+
+
+logapproxchoose <- Vectorize(function(n, k) {
+    if(n <= 6000 && k <= 140)  # chosen by hand
+        return(log(choose(n, k)))
+
+    # Stirling approximation of binomial coefficient
+    return( (0.5 + n)*log(n) - (0.5 + k)*log(k) - (0.5 + n - k)*log(n - k) - 0.5 * log(2 * pi) )
+}, vectorize.args = "k")
+
 
 H.ghype <- function(ensemble = NULL, xi = NULL, omega = NULL, directed = NULL, selfloops = NULL, m = NULL){
   # entropy of the multinomial approximation
@@ -24,17 +34,19 @@ H.ghype <- function(ensemble = NULL, xi = NULL, omega = NULL, directed = NULL, s
   nnzeros <- ps!=0
   ps <- ps[nnzeros]
 
-  pstar <- max(ps)
-  k <- sum(ix)
+  # pstar <- max(ps)
+  # k <- sum(ix)
   if(is.null(m))  m <- ensemble$m
-  x <- seq(10, 140, by=10)
-  xmax <- x[which(log10(k) + log10(choose(m, x)) + log10(x) + log10(log(x)) + x*log10(pstar) < -2)[1]]
-  if (is.na(xmax))
-      xmax <- 140
 
+  xmax <- m %/% 2
   x <- t(matrix(2:xmax, xmax-1, sum(nnzeros)))
+  sums <-  sum( exp(matrix(logapproxchoose(m, x), sum(nnzeros)) + x*log(ps) + log(1-ps)*(m-x) + log(matrix(logfactorial(x), nrow=sum(nnzeros)))) )
+  # while(sums[length(sums)]>1e-200){
+  #     x <- t(matrix((max(x)+1):(max(x)+1), 1, sum(nnzeros)))
+  #     sums <- c(sums, sum( exp(matrix(logapproxchoose(m, x), sum(nnzeros)) + x*log(ps) + log(1-ps)*(m-x) + log(matrix(logfactorial(x), nrow=sum(nnzeros)))) ))
+  # }
 
-  Hval <- - logfactorial(m) - m*sum(ps*log(ps)) + sum( choose(m, x) * ps^x * (1-ps)^(m-x) * matrix(logfactorial(x), nrow=sum(nnzeros)) )
+  Hval <- - logfactorial(m) - m*sum(ps*log(ps)) + sum(sums)
   return('H' = Hval)
 }
 
@@ -55,12 +67,12 @@ maxEntropy <- function(ensemble=NULL, m=NULL, N=NULL, directed=NULL, selfloops=N
 
   ps <- 1/k
 
-  x <- 2:20
-  sums <-  k * choose(m,x) * ps^x * (1-ps)^(m-x) * logfactorial(x)
-  while(sums[length(sums)]>1e-2){
-    x <- (max(x)+1):(max(x)+10)
-    sums <- c(sums, k*choose(m,x)*ps^x * (1-ps)^(m-x) * logfactorial(x))
-  }
+  x <- 2:(m %/% 2)
+  sums <-  k * exp(logapproxchoose(m,x) + x * log(ps) + (m-x) * log(1-ps) + log(logfactorial(x)))
+  # while(sums[length(sums)]>1e-2){
+  #   x <- (max(x)+1):(max(x)+10)
+  #   sums <- c(sums, k*approxchoose(m,x)*ps^x * (1-ps)^(m-x) * logfactorial(x))
+  # }
 
   Hval <- - logfactorial(m) - m*log(ps) + sum(sums)
   return('H' = Hval)
