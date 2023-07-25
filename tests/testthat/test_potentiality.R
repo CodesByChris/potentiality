d33d6a88f9635fca6357813f3f400c6a00f249e1
Christian Zingg <czingg@ethz.ch>
2023-07-25 18:09:45 +0200

    Added tests.

---
+++ tests/testthat/test_potentiality.R
@@ -0,0 +1,123 @@
+# Tests for Potentiality.
+
+library(magrittr)
+library(testthat)
+source("../../R/potentiality.R")  # Remove for package version
+
+
+####################################################################################################
+### Test: Full Network
+####################################################################################################
+
+test_that("Undirected full network has Potentiality 1", {
+    n_nodes <- 30
+    multiplicity <- 5
+    ntwk <- make_empty_graph(n_nodes, directed=FALSE)
+    for (i in 1:vcount(ntwk)) {
+        for (j in 1:vcount(ntwk)) {
+            if (i == j)
+                next
+            ntwk %<>% add_edges(rep(c(i,j), times=multiplicity))
+        }
+    }
+    expect_equal(Potentiality(ntwk), 1)
+})
+
+
+test_that("Directed full network has Potentiality 1", {
+    n_nodes <- 30
+    multiplicity <- 5
+    ntwk <- make_empty_graph(n_nodes, directed=TRUE)
+    for (i in 1:vcount(ntwk)) {
+        for (j in 1:vcount(ntwk)) {
+            if (i == j)
+                next
+            ntwk %<>% add_edges(rep(c(i,j), times=multiplicity))
+        }
+    }
+    expect_equal(Potentiality(ntwk), 1)
+})
+
+
+####################################################################################################
+### Test: Empty Network
+####################################################################################################
+
+test_that("Undirected empty network has Potentiality 0", {
+    n_nodes <- 30
+    ntwk <- make_empty_graph(n_nodes, directed=FALSE)
+    expect_equal(Potentiality(ntwk), 0)
+})
+
+
+test_that("Directed empty network has Potentiality 0", {
+    n_nodes <- 30
+    ntwk <- make_empty_graph(n_nodes, directed=TRUE)
+    expect_equal(Potentiality(ntwk), 0)
+})
+
+
+####################################################################################################
+### Test: Single pair
+####################################################################################################
+
+test_that("Potentiality 0 when only one pair communicates (undirected)", {
+    n_nodes <- 30
+    multiplicity <- 10000
+    ntwk <- make_empty_graph(n_nodes, directed=FALSE)
+    ntwk %<>% add_edges(rep(c(1, 2), times = multiplicity))
+    expect_equal(Potentiality(ntwk), 0)
+})
+
+
+test_that("Potentiality 0 when only one pair communicates (directed)", {
+    n_nodes <- 30
+    multiplicity <- 10000
+    ntwk <- make_empty_graph(n_nodes, directed=TRUE)
+    ntwk %<>% add_edges(rep(c(1, 2), times = multiplicity))
+    expect_equal(Potentiality(ntwk), 0)
+})
+
+
+####################################################################################################
+### Test: Multinomial entropy computation
+####################################################################################################
+
+test_that("Directed network with 2 nodes and strong preference in 1 direction", {
+    weak_multiplicity <- 6
+    strong_multiplicity <- 10000
+    ntwk <- make_empty_graph(2, directed = TRUE)
+    ntwk %<>% add_edges(rep(c(1, 2), times = weak_multiplicity))
+    ntwk %<>% add_edges(rep(c(2, 1), times = strong_multiplicity))
+    expect_equal(Potentiality(ntwk), 0.431235, tolerance = 1E-5)
+    # 0.431235 is computed by applying SciPy's multinomial entropy to the p_ij from Eq.(8) in
+    # DOI:10.3390/e21090901.
+})
+
+
+####################################################################################################
+### Test: Undirected star example from DOI:10.3390/e21090901
+####################################################################################################
+
+test_that("Undirected star", {
+    i_center <- 1
+    i_others <- (1:10)[-i_center]
+    ntwk <- make_empty_graph(10, directed = FALSE)
+    for (j in i_others)
+        ntwk %<>% add_edges(rep(c(i_center, j), times = 10))
+    expect_equal(Potentiality(ntwk), 0.27, tolerance = 1E-2)
+})
+
+
+####################################################################################################
+### Test: Karate club example from DOI:10.3390/e21090901
+####################################################################################################
+
+test_that("Karate club", {
+    data(karate, package = "igraphdata")
+    karate %>%
+        get.adjacency(attr = "weight", sparse = FALSE) %>%
+        graph_from_adjacency_matrix(mode = "undirected") ->
+        karate_network
+    expect_equal(Potentiality(karate_network), 0.31, tolerance = 1E-2)
+})
