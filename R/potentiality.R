6e05707b0cabb4a6053ffd0260413ab80fa080ef
Christian Zingg <czingg@ethz.ch>
2023-08-23 09:49:57 +0200

    Lintr adjustments.

--- R/potentiality.R
+++ R/potentiality.R
@@ -2,29 +2,29 @@
 
 
 #' Compute vector v, where v_i := sum_{p in probs} dbinom(xs_i, m, p)
-#' 
+#'
 #' This function computes the inner sum in eqn.(7) of doi:10.3390/e21090901 for
 #' each x in xs and returns them as a vector whose i'th entry corresponds to
 #' `xs[i]`.
-#' 
+#'
 #' @param xs The values of x in eqn.(7) for which to compute the inner sum.
 #' @param m Number of edges in a network in the ensemble.
 #' @param probs All p_ij in eqn.(7) as a vector. Their ordering does not matter.
 #' @returns The computed vector containing the dbinom sums.
 .dbinom_sum_over_probs <- function(xs, m, probs) {
 
-    # Get unique probabilities and their frequencies
-    probs_counts <- plyr::count(tibble::tibble(.probs = probs), vars = ".probs")
+  # Get unique probabilities and their frequencies
+  probs_counts <- plyr::count(tibble::tibble(.probs = probs), vars = ".probs")
 
-    # Get vector of binom sums over p_ij (entries correspond to xs)
-    binom_sums_per_x <- numeric(length = length(xs))
-    for (i in seq_along(probs_counts$.probs)) {
-        freq <- probs_counts$freq[[i]]
-        prob <- probs_counts$.probs[[i]]
-        binoms <- stats::dbinom(x = xs, size = m, prob = prob)
-        binom_sums_per_x <- binom_sums_per_x + freq * binoms
-    }
-    return(binom_sums_per_x)
+  # Get vector of binom sums over p_ij (entries correspond to xs)
+  binom_sums_per_x <- numeric(length = length(xs))
+  for (i in seq_along(probs_counts$.probs)) {
+    freq <- probs_counts$freq[[i]]
+    prob <- probs_counts$.probs[[i]]
+    binoms <- stats::dbinom(x = xs, size = m, prob = prob)
+    binom_sums_per_x <- binom_sums_per_x + freq * binoms
+  }
+  return(binom_sums_per_x)
 }
 
 
@@ -41,59 +41,59 @@
 entropy_ghype <- function(ensemble = NULL, xi = NULL, omega = NULL,
                           directed = NULL, selfloops = NULL, m = NULL) {
 
-    # Validate args
-    if (is.null(ensemble) && (is.null(xi) || is.null(omega) ||
-                              is.null(directed) || is.null(selfloops) ||
-                              is.null(m)))
-        stop("Either `ensemble` or separate params must be given.")
+  # Validate args
+  if (is.null(ensemble) && (is.null(xi) || is.null(omega) ||
+                            is.null(directed) || is.null(selfloops) ||
+                            is.null(m)))
+    stop("Either `ensemble` or separate params must be given.")
 
-    if (!is.null(ensemble)) {
-        if (is.null(xi))
-            xi <- ensemble$xi
-        if (is.null(omega))
-            omega <- ensemble$omega
-        if (is.null(directed))
-            directed <- ensemble$directed
-        if (is.null(selfloops))
-            selfloops <- ensemble$selfloops
-        if (is.null(m))
-            m <- ensemble$m
-    }
-
-
-    # Get xi and omega
-    ix <- ghypernet::mat2vec.ix(xi, directed, selfloops)
-    xi <- xi[ix]
-    omega <- omega[ix]
-
-    # Compute the ps (according to Eq.(8) in DOI:10.3390/e21090901)
-    ps <- xi * omega / sum(xi * omega)
-    ps <- ps[ps != 0]
-    # Note: For empirical systems with many disconnected components in the
-    #     interaction network this filtering tremendously speeds up the
-    #     computation, given that most entries of ps are zero due to the
-    #     disconnected components.
+  if (!is.null(ensemble)) {
+    if (is.null(xi))
+      xi <- ensemble$xi
+    if (is.null(omega))
+      omega <- ensemble$omega
+    if (is.null(directed))
+      directed <- ensemble$directed
+    if (is.null(selfloops))
+      selfloops <- ensemble$selfloops
+    if (is.null(m))
+      m <- ensemble$m
+  }
 
 
-    # Special Case: 1 Probability
-    if (length(ps) == 1) {
-        if (ps != 1)
-            stop("Single probability != 1 encountered: ", ps)
-        return(0)
-    }
+  # Get xi and omega
+  ix <- ghypernet::mat2vec.ix(xi, directed, selfloops)
+  xi <- xi[ix]
+  omega <- omega[ix]
 
-    # Compute last term (using a trick from scipy with a nested binomial PMF,
-    # see github.com/scipy/scipy/blob/v1.4.1/scipy/stats/_multivariate.py#L3158)
-    xs <- 2:m
-    last_term <- sum(lfactorial(xs) * .dbinom_sum_over_probs(xs, m, ps))
+  # Compute the ps (according to Eq.(8) in DOI:10.3390/e21090901)
+  ps <- xi * omega / sum(xi * omega)
+  ps <- ps[ps != 0]
+  # Note: For empirical systems with many disconnected components in the
+  #     interaction network this filtering tremendously speeds up the
+  #     computation, given that most entries of ps are zero due to the
+  #     disconnected components.
 
-    # Compute the entropy (Eq.(7) in DOI:10.3390/e21090901)
-    return(-lfactorial(m) - m * sum(ps * log(ps)) + last_term)
+
+  # Special Case: 1 Probability
+  if (length(ps) == 1) {
+    if (ps != 1)
+      stop("Single probability != 1 encountered: ", ps)
+    return(0)
+  }
+
+  # Compute last term (using a trick from scipy with a nested binomial PMF,
+  # see github.com/scipy/scipy/blob/v1.4.1/scipy/stats/_multivariate.py#L3158)
+  xs <- 2:m
+  last_term <- sum(lfactorial(xs) * .dbinom_sum_over_probs(xs, m, ps))
+
+  # Compute the entropy (Eq.(7) in DOI:10.3390/e21090901)
+  return(-lfactorial(m) - m * sum(ps * log(ps)) + last_term)
 }
 
 
 #' Maximum entropy of multinomial approximation.
-#' 
+#'
 #' @param ensemble ghype ensemble whose maximum entropy to compute.
 #' @param directed Whether ensemble contains directed networks.
 #' @param selfloops Whether ensemble contains networks with selfloops.
@@ -102,45 +102,45 @@ entropy_ghype <- function(ensemble = NULL, xi = NULL, omega = NULL,
 #' @returns Computed maximum entropy.
 .max_entropy <- function(ensemble = NULL, directed = NULL, selfloops = NULL,
                          m = NULL, n = NULL) {
-    # Validate args
-    if (is.null(ensemble) && (is.null(directed) || is.null(selfloops) ||
-                              is.null(m) || is.null(n)))
-        stop("Either `ensemble` or separate params must be given.")
-    
-    if (!is.null(ensemble)) {
-        if (is.null(directed))
-            directed <- ensemble$directed
-        if (is.null(selfloops))
-            selfloops <- ensemble$selfloops
-        if (is.null(m))
-            m <- ensemble$m
-        if (is.null(n))
-            n <- nrow(ensemble$xi)
-    }
-    
-    ix <- ghypernet::mat2vec.ix(matrix(0, n, n), directed, selfloops)
+  # Validate args
+  if (is.null(ensemble) && (is.null(directed) || is.null(selfloops) ||
+                            is.null(m) || is.null(n)))
+    stop("Either `ensemble` or separate params must be given.")
 
-    # Compute the ps (according to Eq.(9), i.e. all node-pairs equally likely)
-    k <- sum(ix)
-    p <- 1 / k
+  if (!is.null(ensemble)) {
+    if (is.null(directed))
+      directed <- ensemble$directed
+    if (is.null(selfloops))
+      selfloops <- ensemble$selfloops
+    if (is.null(m))
+      m <- ensemble$m
+    if (is.null(n))
+      n <- nrow(ensemble$xi)
+  }
 
-    # Compute the entropy (using a numpy-trick as in entropy_ghype above)
-    xs <- 2:m
-    binoms <- stats::dbinom(x = xs, size = m, prob = p)
-    last_term <- k * sum(lfactorial(xs) * binoms)
-    return(-lfactorial(m) - m * log(p) + last_term)
+  ix <- ghypernet::mat2vec.ix(matrix(0, n, n), directed, selfloops)
+
+  # Compute the ps (according to Eq.(9), i.e. all node-pairs equally likely)
+  k <- sum(ix)
+  p <- 1 / k
+
+  # Compute the entropy (using a numpy-trick as in entropy_ghype above)
+  xs <- 2:m
+  binoms <- stats::dbinom(x = xs, size = m, prob = p)
+  last_term <- k * sum(lfactorial(xs) * binoms)
+  return(-lfactorial(m) - m * log(p) + last_term)
 }
 
 
 #' Computes the ensemble's entropy normalized by the theoretical maximum.
-#' 
+#'
 #' @param ensemble ghype ensemble whose relative entropy to compute.
 #' @returns Relative entropy as a value in the interval between 0 and 1.
 .relative_entropy <- function(ensemble) {
-    entropy <- entropy_ghype(ensemble)
-    if (entropy == 0)
-        return(0)
-    return(entropy / .max_entropy(ensemble))
+  entropy <- entropy_ghype(ensemble)
+  if (entropy == 0)
+    return(0)
+  return(entropy / .max_entropy(ensemble))
 }
 
 
@@ -167,28 +167,28 @@ potentiality <- function(network,
                          has_selfloops = any(igraph::which_loop(network)),
                          full_model = TRUE) {
 
-    # Handle empty networks
-    if (igraph::vcount(network) == 0 || igraph::ecount(network) == 0)
-        return(0)
+  # Handle empty networks
+  if (igraph::vcount(network) == 0 || igraph::ecount(network) == 0)
+    return(0)
 
-    # Compute params
-    if (has_selfloops)
-        stop("ERROR: Potentiality not implemented for selfloops.")
+  # Compute params
+  if (has_selfloops)
+    stop("ERROR: Potentiality not implemented for selfloops.")
 
-    # Compute Potentiality
-    if (isTRUE(full_model)) {
-        # Use full ghype propensities
-        ens <- ghypernet::ghype(network, directed = directed,
-                                selfloops = has_selfloops, unbiased = FALSE)
-    } else {
-        # Use bccm with inferred blocks
-        adj <- igraph::get.adjacency(network, sparse = FALSE)
-        net <- igraph::graph_from_adjacency_matrix(adj, weighted = TRUE)
-        labs <- igraph::membership(igraph::cluster_fast_greedy(
-            graph = igraph::as.undirected(net), modularity = FALSE
-        ))
-        ens <- ghypernet::bccm(adj, labels = labs, directed = directed,
-                               selfloops = has_selfloops, ignore_pvals = TRUE)
-    }
-    return(.relative_entropy(ens))
+  # Compute Potentiality
+  if (isTRUE(full_model)) {
+    # Use full ghype propensities
+    ens <- ghypernet::ghype(network, directed = directed,
+                            selfloops = has_selfloops, unbiased = FALSE)
+  } else {
+    # Use bccm with inferred blocks
+    adj <- igraph::get.adjacency(network, sparse = FALSE)
+    net <- igraph::graph_from_adjacency_matrix(adj, weighted = TRUE)
+    labs <- igraph::membership(igraph::cluster_fast_greedy(
+      graph = igraph::as.undirected(net), modularity = FALSE
+    ))
+    ens <- ghypernet::bccm(adj, labels = labs, directed = directed,
+                           selfloops = has_selfloops, ignore_pvals = TRUE)
+  }
+  return(.relative_entropy(ens))
 }
--- tests/testthat/test_potentiality.R
+++ tests/testthat/test_potentiality.R
@@ -8,32 +8,32 @@ library(magrittr)
 ################################################################################
 
 test_that("Undirected full network has potentiality 1", {
-    n_nodes <- 30
-    multiplicity <- 5
-    ntwk <- igraph::make_empty_graph(n_nodes, directed = FALSE)
-    for (i in 1:igraph::vcount(ntwk)) {
-        for (j in 1:igraph::vcount(ntwk)) {
-            if (i == j)
-                next
-            ntwk %<>% igraph::add_edges(rep(c(i, j), times = multiplicity))
-        }
+  n_nodes <- 30
+  multiplicity <- 5
+  ntwk <- igraph::make_empty_graph(n_nodes, directed = FALSE)
+  for (i in 1:igraph::vcount(ntwk)) {
+    for (j in 1:igraph::vcount(ntwk)) {
+      if (i == j)
+        next
+      ntwk %<>% igraph::add_edges(rep(c(i, j), times = multiplicity))
     }
-    expect_equal(potentiality(ntwk), 1)
+  }
+  expect_equal(potentiality(ntwk), 1)
 })
 
 
 test_that("Directed full network has potentiality 1", {
-    n_nodes <- 30
-    multiplicity <- 5
-    ntwk <- igraph::make_empty_graph(n_nodes, directed = TRUE)
-    for (i in 1:igraph::vcount(ntwk)) {
-        for (j in 1:igraph::vcount(ntwk)) {
-            if (i == j)
-                next
-            ntwk %<>% igraph::add_edges(rep(c(i, j), times = multiplicity))
-        }
+  n_nodes <- 30
+  multiplicity <- 5
+  ntwk <- igraph::make_empty_graph(n_nodes, directed = TRUE)
+  for (i in 1:igraph::vcount(ntwk)) {
+    for (j in 1:igraph::vcount(ntwk)) {
+      if (i == j)
+        next
+      ntwk %<>% igraph::add_edges(rep(c(i, j), times = multiplicity))
     }
-    expect_equal(potentiality(ntwk), 1)
+  }
+  expect_equal(potentiality(ntwk), 1)
 })
 
 
@@ -42,16 +42,16 @@ test_that("Directed full network has potentiality 1", {
 ################################################################################
 
 test_that("Undirected empty network has potentiality 0", {
-    n_nodes <- 30
-    ntwk <- igraph::make_empty_graph(n_nodes, directed = FALSE)
-    expect_equal(potentiality(ntwk), 0)
+  n_nodes <- 30
+  ntwk <- igraph::make_empty_graph(n_nodes, directed = FALSE)
+  expect_equal(potentiality(ntwk), 0)
 })
 
 
 test_that("Directed empty network has potentiality 0", {
-    n_nodes <- 30
-    ntwk <- igraph::make_empty_graph(n_nodes, directed = TRUE)
-    expect_equal(potentiality(ntwk), 0)
+  n_nodes <- 30
+  ntwk <- igraph::make_empty_graph(n_nodes, directed = TRUE)
+  expect_equal(potentiality(ntwk), 0)
 })
 
 
@@ -60,20 +60,20 @@ test_that("Directed empty network has potentiality 0", {
 ################################################################################
 
 test_that("potentiality 0 when only one pair communicates (undirected)", {
-    n_nodes <- 30
-    multiplicity <- 10000
-    ntwk <- igraph::make_empty_graph(n_nodes, directed = FALSE)
-    ntwk %<>% igraph::add_edges(rep(c(1, 2), times = multiplicity))
-    expect_equal(potentiality(ntwk), 0)
+  n_nodes <- 30
+  multiplicity <- 10000
+  ntwk <- igraph::make_empty_graph(n_nodes, directed = FALSE)
+  ntwk %<>% igraph::add_edges(rep(c(1, 2), times = multiplicity))
+  expect_equal(potentiality(ntwk), 0)
 })
 
 
 test_that("potentiality 0 when only one pair communicates (directed)", {
-    n_nodes <- 30
-    multiplicity <- 10000
-    ntwk <- igraph::make_empty_graph(n_nodes, directed = TRUE)
-    ntwk %<>% igraph::add_edges(rep(c(1, 2), times = multiplicity))
-    expect_equal(potentiality(ntwk), 0)
+  n_nodes <- 30
+  multiplicity <- 10000
+  ntwk <- igraph::make_empty_graph(n_nodes, directed = TRUE)
+  ntwk %<>% igraph::add_edges(rep(c(1, 2), times = multiplicity))
+  expect_equal(potentiality(ntwk), 0)
 })
 
 
@@ -82,14 +82,14 @@ test_that("potentiality 0 when only one pair communicates (directed)", {
 ################################################################################
 
 test_that("Directed network with 2 nodes and unidirectional preference", {
-    weak_multiplicity <- 6
-    strong_multiplicity <- 10000
-    ntwk <- igraph::make_empty_graph(2, directed = TRUE)
-    ntwk %<>% igraph::add_edges(rep(c(1, 2), times = weak_multiplicity))
-    ntwk %<>% igraph::add_edges(rep(c(2, 1), times = strong_multiplicity))
-    expect_equal(potentiality(ntwk), 0.431235, tolerance = 1E-5)
-    # 0.431235 is computed by applying SciPy's multinomial entropy to the p_ij
-    # from Eq.(8) in DOI:10.3390/e21090901.
+  weak_multiplicity <- 6
+  strong_multiplicity <- 10000
+  ntwk <- igraph::make_empty_graph(2, directed = TRUE)
+  ntwk %<>% igraph::add_edges(rep(c(1, 2), times = weak_multiplicity))
+  ntwk %<>% igraph::add_edges(rep(c(2, 1), times = strong_multiplicity))
+  expect_equal(potentiality(ntwk), 0.431235, tolerance = 1E-5)
+  # 0.431235 is computed by applying SciPy's multinomial entropy to the p_ij
+  # from Eq.(8) in DOI:10.3390/e21090901.
 })
 
 
@@ -98,13 +98,13 @@ test_that("Directed network with 2 nodes and unidirectional preference", {
 ################################################################################
 
 test_that("Undirected star", {
-    i_center <- 1
-    i_others <- (1:10)[-i_center]
-    ntwk <- igraph::make_empty_graph(10, directed = FALSE)
-    for (j in i_others)
-        ntwk %<>% igraph::add_edges(rep(c(i_center, j), times = 10))
-    pot <- potentiality(ntwk)
-    expect_true(0.265 <= pot && pot <= 0.275)
+  i_center <- 1
+  i_others <- (1:10)[-i_center]
+  ntwk <- igraph::make_empty_graph(10, directed = FALSE)
+  for (j in i_others)
+    ntwk %<>% igraph::add_edges(rep(c(i_center, j), times = 10))
+  pot <- potentiality(ntwk)
+  expect_true(0.265 <= pot && pot <= 0.275)
 })
 
 
@@ -113,10 +113,10 @@ test_that("Undirected star", {
 ################################################################################
 
 test_that("Karate club", {
-    data(karate, package = "igraphdata")
-    pot <- karate %>%
-        igraph::get.adjacency(attr = "weight", sparse = FALSE) %>%
-        igraph::graph_from_adjacency_matrix(mode = "undirected") %>%
-        potentiality()
-    expect_true(0.305 <= pot && pot <= 0.315)
+  data(karate, package = "igraphdata")
+  pot <- karate %>%
+    igraph::get.adjacency(attr = "weight", sparse = FALSE) %>%
+    igraph::graph_from_adjacency_matrix(mode = "undirected") %>%
+    potentiality()
+  expect_true(0.305 <= pot && pot <= 0.315)
 })
